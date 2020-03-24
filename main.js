44d79ef7022d9d269ab02f17351cc6b1a86ce1a8
kristianpedersen <kristianp89@gmail.com>
2020-03-24 15:44:15 +0100

    Forbedringer

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 const ansatte = "bjørg, eirik, elisabeth, elisabethbr, gavin, hanne, ingunn, kathrine, kristian, larsharald, merethe, tim, tommy, øivind".split(", ")
 
+const status = Array(ansatte.length).fill(false)
+
+const images = ansatte.map((navn, index) => {
+	const img = document.createElement("img")
+	img.onload = function () {
+		status[index] = true
+	}
+	img.src = navn + ".jpg"
+	return img
+})
+
 let i = 0
 let t = 0
 let cycle = true
@@ -11,7 +22,7 @@ const cross = document.querySelector("span")
 loop()
 
 function loop() {
-	if (new Date() - t > 500 && cycle) {
+	if (new Date() - t > 500 && cycle && status.every(image => image === true)) {
 		t = new Date()
 		img1.src = `${ansatte[i]}.jpg`
 		img2.src = `${ansatte[(i + ansatte.length / 2) % ansatte.length]}.jpg`
